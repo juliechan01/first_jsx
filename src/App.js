78cae4466191bb538a5e02a7bd71ce6531847129
Julie Chan <juliechan03@gmail.com>
2023-05-03 18:31:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <p>* Learn React</p>
+      <p>* Climb Mt Everest</p>
+      <p>* Run a marathon</p>
+      <p>* Cry in a corner because I don't have any dogs...</p>
+    </div>
+  );
+}
+
+export default App;
